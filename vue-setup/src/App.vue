23cf51fc66ae3136774622016dadd10f74b601cb
claudiacastella <claudiacastella1984@gmail.com>
2022-12-09 18:25:25 +0100

    Iterations 1-3

---
+++ vue-setup/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+   <Button @click="sayHello" />
+   <p :class="toshow"> {{helloWorld()}}</p>
+  </div>
+
+</template>
+
+<script setup>
+import Button from "./components/Button.vue" ; 
+import {ref} from "vue";
+
+function helloWorld(){
+  return `Hello World`;
+}
+const toshow = ref("hidden")
+
+function sayHello() {
+    toshow.value === "hidden" ? toshow.value = "show" : toshow.value = "hidden";
+    return toshow.value;
+}
+
+</script>
+
+<style>
+.show{
+  display: block;
+}
+.hidden {
+  display: none;
+}
+</style>
---
+++ vue-setup/src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button>Hi</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+
+
+</script>
+
+<style>
+
+</style>
